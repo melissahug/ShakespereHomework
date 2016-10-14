0c5f4b79efa9e37a831a84e666a4aeecebe40e33
Melissa <hughes.melissa1@gmail.com>
2016-10-13 20:25:46 -0400

    first commit/homework

---
+++ ShakespereHomework/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ShakespereHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Read and print the first line from the file only.
+            Read and print the entire text file.
+            Read and print the entire text file and capitalize all lowercase vowels.*/
+
+            //StreamReader reader = new StreamReader("C:\\Users\\WeCanCodeIT\\Documents\\Visual Studio 2015\\Projects\\Shakespere.txt");
+
+            //This prints line one only
+            /*string newLine = reader.ReadLine();
+            Console.WriteLine("Line 1: " +newLine);
+            */
+            
+            StreamReader reader = new StreamReader("C:\\Users\\WeCanCodeIT\\Documents\\Visual Studio 2015\\Projects\\Shakespere.txt");
+            string line = reader.ReadLine();
+
+            int lineNumber = 1;
+            
+
+            while (line != null)
+            {
+                lineNumber++;
+                Console.WriteLine(lineNumber);
+                lineNumber = lineTwo.ReadLine();
+            }
+            lineTwo.Close();
+           
+            
+
+
+        
+    }
+}
